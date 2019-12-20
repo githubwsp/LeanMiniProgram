96831e846f3179d6c9c6422ab72d29fa8543fc93
masterWu <wusongpingsmile@gmail.com>
2019-12-20 22:38:27 +0800

    title and tabbar

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,82 +5,88 @@ Page({
    * 页面的初始数据
    */
   data: {
-    name:'吴宋平',
-    study: [{ id: 1, date:'2019年12月20日'},{id:2,date:'2019年12月21日'}],
-    count:0
+    name: '吴宋平',
+    study: [{
+      id: 1,
+      date: '2019年12月20日'
+    }, {
+      id: 2,
+      date: '2019年12月21日'
+    }],
+    count: 0
   },
 
   /**
    * 按键监听事件
    */
   onButtonClick() {
-      this.setData({
-        count: this.data.count + 1
-      })
+    this.setData({
+      count: this.data.count + 1
+    })
   },
 
   /**
    * 清除点击次数
    */
-  onCleanClick(){
+  onCleanClick() {
     this.setData({
-      count:this.data.count = 0
+      count: this.data.count = 0
     })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
